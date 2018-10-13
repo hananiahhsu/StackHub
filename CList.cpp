0bf10b052904236e1e0461061a3125b56c236c9b
hananiahhsu <hananiahhsu@126.com>
2018-10-13 18:39:48 +0800

    xu-2018.10.13-18.39

--- CList.cpp
+++ CList.cpp
@@ -1,2 +1,57 @@
 #include "pch.h"
 #include "CList.h"
+
+template <class T>
+void CList<T>::Append(CNode<T>*& node, T* tElement)
+{
+	if (node == NULL)
+	{
+		dwCount++;
+		node = new CNode<T>(tElement);
+		return;
+	}
+	Append(node->Next(),tElement);
+}
+
+//Ç°²å
+template<class T>
+void CList<T>::Insert(T* tElement)
+{
+	dwCount++;
+	if (head == NULL)
+	{
+		head = new CNode<T>(tElement);
+		return;
+	}
+	else
+	{
+		CNode<T> * tmp = head;
+		head = new CNode<T>(tElement);
+		head->next = tmp;
+		return;
+	}
+}
+
+template<class T>
+bool CList<T>::Remove(T* tElement)
+{
+
+}
+
+template<class T>
+T* CList<T>::GetLast()
+{
+
+}
+
+template<class T>
+T* CList<T>::GetNext(T* tElement)
+{
+
+}
+
+template<class T>
+T* CList<T>::Find(DWORD(*Function)(T* tParameter), DWORD dwValue)
+{
+
+}
--- CList.h
+++ CList.h
@@ -26,4 +26,44 @@ private:
 	CNode* next;
 };
 
+
+
+template<class T>
+class CList
+{
+public:
+	CList() :dwCount(0),head(0)
+	{
+
+	}
+	CList(T* tElement):dwCount(1),head(new CNode<T>(tElement))
+	{
+
+	}
+	virtual~CList() {}
+public:
+	void Append(CNode<T>*& node, T* tElement);
+	void Insert(T* tElement);
+	bool Remove(T* tElement);
+	DWORD Count()const { return dwCount; }
+	CNode<T>*& Head() { return head; }
+	T* GetFirst() { return head != NULL ? head->Element() : NULL; }
+	T* GetLast();
+	T* GetNext(T* tElement);
+	T* Find(DWORD(*Function)(T* tParameter),DWORD dwValue);
+protected:
+	CList(const CList& list);
+	CList& operator=(const CList& list);
+private:
+	CNode<T>* head;
+	DWORD dwCount;
+};
+
+
+
+
+
+
+
+
 #endif
